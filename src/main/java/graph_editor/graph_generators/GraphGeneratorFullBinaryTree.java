d80b06a7fc77a9e14a75050c59da8da4577cdb09
tymoteusz <tymoteusz.chelmecki@student.uj.edu.pl>
2023-02-14 19:29:38 +0100

    Reimplement GraphGeneratorFullBinaryTree

--- src/main/java/graph_editor/graph_generators/GraphGeneratorFullBinaryTree.java
+++ src/main/java/graph_editor/graph_generators/GraphGeneratorFullBinaryTree.java
@@ -1,37 +1,39 @@
-//package graph_editor.graph_generators;
-//
-//import com.example.graph_editor.model.GraphType;
-//import com.example.graph_editor.model.mathematics.Geometry;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//public class GraphGeneratorFullBinaryTree implements GraphGenerator {
-//    @Override
-//    public List<Parameter> getParameters() {
-//        List<Parameter> parameters = new ArrayList<>();
-//        parameters.add(new Parameter("Layers", 2, 8));
-//        return parameters;
-//    }
-//
-//    @Override
-//    public Graph generate(List<Integer> parameters) {
-//        int layers = parameters.get(0);
-//        int v = (1 << layers) - 1;
-//        Graph result = new GraphImpl(GraphType.UNDIRECTED);
-//        for (int i = 0; i< v; i++) result.addVertex();
-//
-//        List<Vertex> vertices = result.getVertices();
-//
-//        for (int i = 0; i< v/2; i++) {
-//            result.addEdge(vertices.get(i), vertices.get((i+1)*2-1));
-//            result.addEdge(vertices.get(i), vertices.get((i+1)*2));
-//        }
-//
-//        for (int i = 0; i< v; i++) {
-//            vertices.get(i).setPoint(Geometry.getPointBinaryTree(i, layers));
-//        }
-//
-//        return result;
-//    }
-//}
+package graph_editor.graph_generators;
+
+import graph_editor.geometry.GeometryUtils;
+import graph_editor.graph.UndirectedGraph;
+import graph_editor.visual.BuilderVisualizer;
+import graph_editor.visual.GraphVisualization;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GraphGeneratorFullBinaryTree implements GraphGenerator<UndirectedGraph> {
+    @Override
+    public List<Parameter> getParameters() {
+        List<Parameter> parameters = new ArrayList<>();
+        parameters.add(new Parameter("Layers", 2, 8));
+        return parameters;
+    }
+
+    @Override
+    public GraphVisualization<UndirectedGraph> generate(List<Integer> parameters) {
+        int layers = parameters.get(0);
+        int vertexCount = (1 << layers) - 1;
+        UndirectedGraph.Builder builder = new UndirectedGraph.Builder(vertexCount);
+
+        for (int i = 0; i < vertexCount/2; i++) {
+            builder.addEdge(i, (i+1)*2 - 1);
+            builder.addEdge(i, (i+1)*2);
+        }
+        UndirectedGraph graph = builder.build();
+
+        BuilderVisualizer visualizer = new BuilderVisualizer();
+        for (int i = 0; i < vertexCount; i++) {
+            visualizer.addCoordinates(graph.getVertices().get(i),
+                    GeometryUtils.getPointBinaryTree(i, layers));
+        }
+
+        return visualizer.generateVisual(graph);
+    }
+}
