77b63ab48b58e4e0885ff3e1554e5271f62fd61b
tymoteusz <tymoteusz.chelmecki@student.uj.edu.pl>
2023-02-14 09:28:41 +0100

    Add clique generator

--- src/main/java/graph_editor/graph_generators/GraphGeneratorClique.java
+++ src/main/java/graph_editor/graph_generators/GraphGeneratorClique.java
@@ -1,37 +1,39 @@
-//package graph_editor.graph_generators;
-//
-//import com.example.graph_editor.model.GraphType;
-//import com.example.graph_editor.model.mathematics.Geometry;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//public class GraphGeneratorClique implements GraphGenerator {
-//    @Override
-//    public List<Parameter> getParameters() {
-//        List<Parameter> parameters = new ArrayList<>();
-//        parameters.add(new Parameter("Vertices", 3, 64));
-//        return parameters;
-//    }
-//
-//    @Override
-//    public Graph generate(List<Integer> parameters) {
-//        int v = parameters.get(0);
-//        Graph result = new GraphImpl(GraphType.UNDIRECTED);
-//        for (int i = 0; i< v; i++) result.addVertex();
-//
-//        List<Vertex> vertices = result.getVertices();
-//
-//        for (int i = 0; i< v; i++) {
-//            for (int j = i+1; j<v; j++) {
-//                result.addEdge(vertices.get(i), vertices.get(j));
-//            }
-//        }
-//
-//        for (int i = 0; i< v; i++) {
-//            vertices.get(i).setPoint(Geometry.getPointOnCircle(i, v));
-//        }
-//
-//        return result;
-//    }
-//}
+package graph_editor.graph_generators;
+
+import graph_editor.graph.UndirectedGraph;
+import graph_editor.visual.BuilderVisualizer;
+import graph_editor.visual.GraphVisualization;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GraphGeneratorClique implements GraphGenerator<UndirectedGraph> {
+
+    @Override
+    public List<Parameter> getParameters() {
+        List<Parameter> parameters = new ArrayList<>();
+        parameters.add(new Parameter("Vertices", 3, 64));
+        return parameters;
+    }
+
+    @Override
+    public GraphVisualization<UndirectedGraph> generate(List<Integer> parameters) {
+        int cliqueSize = parameters.get(0);
+        UndirectedGraph.Builder builder = new UndirectedGraph.Builder(cliqueSize);
+
+        for (int i = 0; i < cliqueSize; i++) {
+            for (int j = i + 1; j < cliqueSize; j++) {
+                builder.addEdge(i, j);
+            }
+        }
+        UndirectedGraph graph = builder.build();
+
+        BuilderVisualizer visualizer = new BuilderVisualizer();
+        for (int i = 0; i < cliqueSize; i++) {
+            visualizer.addCoordinates(graph.getVertices().get(i),
+                    GeometryHelper.getPointOnCycle(i, cliqueSize));
+        }
+
+        return visualizer.generateVisual(graph);
+    }
+}
